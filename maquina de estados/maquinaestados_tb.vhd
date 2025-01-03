768f7d1ae6c2cc4fa3dac936b9186fe8893fad7e
nuria0509 <80456455+nuria0509@users.noreply.github.com>
2025-01-03 19:32:46 +0100

    Update maquinaestados_tb.vhd

--- maquina de estados/maquinaestados_tb.vhd
+++ maquina de estados/maquinaestados_tb.vhd
@@ -3,25 +3,25 @@ use IEEE.Std_logic_1164.all;
 use IEEE.Numeric_Std.all;
 
 entity top_tb is
-end;
+end top_tb;
 
 architecture bench of top_tb is
 
   -- Declaración del componente
   component top
     port (
-      reset : in std_logic;
-      CLK   : in std_logic;
-      button: in std_logic_vector(0 to 4);
-      led   : out std_logic_vector(0 to 4)
+      reset  : in std_logic;
+      CLK    : in std_logic;
+      button : in std_logic_vector(4 downto 0);
+      led    : out std_logic_vector(4 downto 0)
     );
   end component;
 
   -- Señales internas
-  signal reset : std_logic := '0';
-  signal CLK   : std_logic := '0';
-  signal button: std_logic_vector(0 to 4) := (others => '0');
-  signal led   : std_logic_vector(0 to 4);
+  signal reset  : std_logic := '0';
+  signal CLK    : std_logic := '0';
+  signal button : std_logic_vector(4 downto 0) := (others => '0');
+  signal led    : std_logic_vector(4 downto 0);
 
 begin
 
@@ -53,7 +53,8 @@ begin
     wait for 20 ns;
     reset <= '0';
 
-    -- Simulación de pulsaciones de botón
+    -- Estímulos de prueba
+    -- Pulsación de botones simulada
     button <= "10000";
     wait for 50 ns;
 
@@ -69,6 +70,11 @@ begin
     button <= "00001";
     wait for 50 ns;
 
+    -- Prueba de reset en funcionamiento
+    reset <= '1';
+    wait for 20 ns;
+    reset <= '0';
+
     -- Finalización de la simulación
     wait;
   end process;
@@ -76,11 +82,12 @@ begin
 end bench;
 
 -- Configuración de la simulación
-configuration cfg_top_tb_Behavioral of top_tb is
+configuration cfg_top_tb of top_tb is
   for bench
     for uut: top
       use entity work.top(Behavioral);
     end for;
   end for;
-end cfg_top_tb_Behavioral;
+end cfg_top_tb;
+
 
